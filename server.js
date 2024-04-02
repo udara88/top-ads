9629dc1f75c567c18efb52b093f4a7063d8939ee
udara88 <udarawickramasinghe88@gmail.com>
2024-04-02 13:47:51 +0530

    connect databse and create user model

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-const express = require('express')
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv' 
+dotenv.config();
+
 const app = express()
 
-const posts = [
-    {
-        username:'isuru',
-        title:'Post 1'
-    },
-    {
-        username:'dewanga',
-        title:'Post 2'
-    }
-]
+const MONGO_URI = process.env.MONGO_DB_URL
 
+mongoose.connect(MONGO_URI).then(()=>{
+    console.log('Mongodb is connected')
+})
+.catch((err)=>{
+    console.log(err)
+
+})
 app.listen(3001,()=>{
     console.log('Server is runing on port 3001')
 })
